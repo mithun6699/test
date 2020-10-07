245a6b2eae63a6fe2feef77482d33eef6c66be68
= <=>
2020-10-07 17:47:00 +0530

    training day 0

---
+++ script.js
@@ -0,0 +1,43 @@
+// alert("Mithun");
+// document.getElementById("gt").addEventListener("mouseover", function () {
+//   alert("wow");
+// });
+
+// let gtm = document.getElementById("gt");
+// gtm.addEventListener("click", () => {
+//   alert("Mithun");
+// });
+
+// document.getElementById("heading").textContent = " Mithun";
+// console.log(Math.random(99));
+// console.log(Number(7));
+// let a = 7;
+// console.log(Number(typeof a));
+// for (let i = 0; i <= 5; i++) {
+//   setTimeout(() => {
+//     console.log("a");
+//   }, 2000);
+// }
+// setInterval(() => {
+//   console.log("HIII");
+// }, 5000);
+// for (var i = 0; i <= 5; i++) {
+//   setTimeout(() => {
+//     console.log("a");
+//   }, 2000);
+// }
+// console.log(i);
+// console.log(Math.random() > 0.9 ? console.log("mithu") : console.log("Raj"));
+
+// console.log("Mithunnn");
+// function mithun() {
+//   console.log("Hiii");
+// }
+// mithun();
+// console.log("RAj");
+// console.log("Mithunnn");
+// setTimeout(() => {
+//   console.log("HGHG");
+// }, 5000);
+
+// console.log("Raj");
